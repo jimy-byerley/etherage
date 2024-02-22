09719732da669c941faaf894f2faaea2efc37453
Jimy Byerley <jimy.byerley@gmail.com>
2024-02-22 15:24:35 +0100

    added examples using servodrives

--- examples/servodrive_regulation.rs
+++ examples/servodrive_regulation.rs
@@ -1,11 +1,11 @@
 use std::error::Error;
 use core::time::Duration;
-use futures_concurrency::future::Join;
+use futures_concurrency::future::Race;
 use etherage::{
     EthernetSocket, Master,
     Slave, SlaveAddress, CommunicationState,
     data::Field,
-    sdo::{self, OperationMode},
+    sdo::{self, Sdo, OperationMode},
     mapping::{self, Mapping, Group},
     registers::{self, SyncDirection},
     };
@@ -14,7 +14,6 @@ use etherage::{
 async fn main() -> Result<(), Box<dyn Error>> {
     let master = Master::new(EthernetSocket::new("eno1")?);
     
-    println!("create mapping");
     let config = mapping::Config::default();
     let mapping = Mapping::new(&config);
     let mut slave = mapping.slave(1);
@@ -24,18 +23,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 let controlword      = pdo.push(sdo::cia402::controlword);
                 let target_mode      = pdo.push(sdo::cia402::target::mode);
                 let target_position  = pdo.push(sdo::cia402::target::position);
+                let offset_torque    = pdo.push(sdo::cia402::offset::torque);
         let mut channel = slave.channel(sdo::sync_manager.logical_read(), 0x1c00 .. 0x2000);
             let mut pdo = channel.push(sdo::Pdo::with_capacity(0x1a00, false, 10));
                 let statusword       = pdo.push(sdo::cia402::statusword);
                 let error            = pdo.push(sdo::cia402::error);
                 let current_position = pdo.push(sdo::cia402::current::position);
     drop(slave);
-    println!("done {:#?}", config);
-    
     let group = master.group(&mapping);
-    
-    println!("group {:#?}", group);
-    println!("fields {:#?}", (statusword, controlword));
+
+    let kpp = Sdo::<f32>::sub(0x2012, 1, 0);   // position propotional gain
+    let kvp = Sdo::<f32>::sub(0x2012, 5, 0);   // velocity propotional gain
+    let kvi = Sdo::<f32>::sub(0x2012, 6, 0);   // velocity integral gain
 
     let mut slave = Slave::new(&master, SlaveAddress::AutoIncremented(0)).await?;
     slave.switch(CommunicationState::Init).await?;
@@ -49,10 +48,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     slave.switch(CommunicationState::Operational).await?;
 
     let cycle = tokio::sync::Notify::new();
+    let period = 1e-3; // refreshing period (s)
 
     (
         async {
-            let mut period = tokio::time::interval(Duration::from_millis(1));
+            let mut period = tokio::time::interval(Duration::from_secs_f32(period));
             loop {
                 group.data().await.exchange().await;
                 cycle.notify_waiters();
@@ -62,55 +62,66 @@ async fn main() -> Result<(), Box<dyn Error>> {
         async {
             let initial = {
                 let mut group = group.data().await;
-
                 let initial = group.get(current_position);
                 group.set(target_mode, OperationMode::SynchronousPosition);
                 group.set(target_position, initial);
-
-                println!("error {:x}", group.get(error));
-                println!("position  {}", initial);
-                initial
+                initial as f32
             };
-
-            use bilge::prelude::u2;
-            println!("power: {:?}", slave.coe().await.sdo_read(&sdo::error, u2::new(0)).await);
 
             println!("switch on");
             switch_on(statusword, controlword, error, &group, &cycle).await;
 
-            // coder with 2^18 pulses/rotation
-// 			let increment = 3_000_000;
-// 			let course = 100_000_000;
-            // coder with less
-			let increment = 1_000;
-			let course = 100_000;
+            let position_frequency = 0.05;  // sine frequency (Hz)
+            let position_amplitude = 200e3; // sine amplitude (coder pulses)
+            let torque_frequency = 4.;  // sine frequency (Hz)
+            let torque_amplitude = 400.; // sine amplitude (rated/1000)
+            let mut t = 0.;
 
-
-			println!("move forward");
+			println!("move");
 			loop {
+
                 cycle.notified().await;
                 let mut group = group.data().await;
 
-                let position = group.get(current_position);
-                if position >= initial + course {break}
-                group.set(target_position, position + increment);
-                println!("    {}", position);
+                group.set(target_position, (initial + (t*position_frequency*2.*std::f32::consts::PI).sin() * position_amplitude) as _);
+                group.set(offset_torque,   ((t*torque_frequency*2.*std::f32::consts::PI).sin() * torque_amplitude) as _);
+
+                t += period;
             }
-
-			println!("move backward");
-            loop {
-                cycle.notified().await;
-                let mut group = group.data().await;
-
-                let position = group.get(current_position);
-                if position <= initial {break}
-                group.set(target_position, position - increment);
-                println!("    {}", position);
-            }
-
-            println!("done");
         },
-    ).join().await;
+        async {
+            use bilge::prelude::u2;
+            let priority = u2::new(0);
+
+            println!("initial {} {} {}",
+                slave.coe().await.sdo_read(&kpp, priority).await.unwrap(),
+                slave.coe().await.sdo_read(&kvp, priority).await.unwrap(),
+                slave.coe().await.sdo_read(&kvi, priority).await.unwrap(),
+                );
+
+            let regulator_period = 10.;
+
+            loop {
+                println!("low");
+                slave.coe().await.sdo_write(&kvp, priority, 0.5).await.unwrap();
+                slave.coe().await.sdo_write(&kvp, priority, 0.6).await.unwrap();
+                slave.coe().await.sdo_write(&kvi, priority, 0.1).await.unwrap();
+                tokio::time::sleep(Duration::from_secs_f32(regulator_period)).await;
+
+                println!("med");
+                slave.coe().await.sdo_write(&kvp, priority, 1.).await.unwrap();
+                slave.coe().await.sdo_write(&kvp, priority, 2.).await.unwrap();
+                slave.coe().await.sdo_write(&kvi, priority, 0.1).await.unwrap();
+                tokio::time::sleep(Duration::from_secs_f32(regulator_period)).await;
+
+                println!("high");
+                slave.coe().await.sdo_write(&kvp, priority, 4.).await.unwrap();
+                slave.coe().await.sdo_write(&kvp, priority, 6.).await.unwrap();
+                slave.coe().await.sdo_write(&kvi, priority, 1.).await.unwrap();
+                tokio::time::sleep(Duration::from_secs_f32(regulator_period)).await;
+            }
+        },
+    ).race().await;
 
     Ok(())
 }
